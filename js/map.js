57a965a0bb6d6a53d041f85faca0fa73c349dbf1
EvgeniaOstapenko <ostapenko5.ru@mail.ru>
2017-12-02 11:13:08 +1000

    second and third tasks

--- js/map.js
+++ js/map.js
@@ -12,34 +12,78 @@ var checkin = ["12:00", "13:00", "14:00"];
 var checkout = ["12:00", "13:00", "14:00"];
 var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
-for (i = 1, i <= 8, i++) {
+var arr = [];
+
+for (i = 1; i <= 8; i++) {
   var index = random(1,8);
   var obj = {
     "author": {
       "avatar": 'img/avatars/user0' + i + '.png'
     },
 
+	"location":{
+	 "x":random(300, 900),
+	 "y":random(100, 500)
+   },
+   
     "offer": {
       "title":title[index],
-      "address": location.x.toString()+', '+location.y.toString(),
+      "address": function() {
+		  return location.x.toString()+', '+location.y.toString()
+	  },
       "price": random(1000, 1000000),
-    "type":type[index] ,
-    "rooms": random(1, 5),
-  "guests": random(1, 8),
-  "checkin":checkin[index],
-    "checkout":checkout[index],
-    "features":features[index],
-    "description":'',
-    "photos":[],
-   },
-
-  "location":{
-    "x":random(300, 900),
-    "y":random(100, 500)
-  }
- };
+      "type":type[index] ,
+      "rooms": random(1, 5),
+      "guests": random(1, 8),
+      "checkin":checkin[index],
+      "checkout":checkout[index],
+      "features":features[index],
+      "description":'',
+      "photos":[],
+   }
+   
+  };
+  
+  arr.push(obj);
 };
 
-.map.classList.remove('map--faded');
+
+var el = document.querySelector(".map--faded");
+el.classList.remove("map--faded");
+
+var fragment = document.createDocumentFragment();
+
+for (i = 0; i < 8; i++){	
+	
+	var button = document.createElement("button");
+	button.className = "map__pin";
+	button.setAttribute("style", "left:" + arr[i].location.x + 'px' + ", top:" + arr[i].location.y + 'px');
+
+	var img = document.createElement("img");
+	img.setAttribute("scr", arr[i].author.avatar);
+	img.setAttribute("width", "40");
+	img.setAttribute("height", "40");
+	img.setAttribute("draggable","false");
+	
+	button.appendChild(img);
+	
+	fragment.appendChild(button);
+};
+
+var insertEl = document.querySelector(".map__pins");
+insertEl.appendChild(fragment); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
